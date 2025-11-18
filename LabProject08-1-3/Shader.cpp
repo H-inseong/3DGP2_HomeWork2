2d2533a7441e8e5ab85a4733bd2f98d1de6a02e1
hwanginseong@laptop1 <3dlstjd3@gmail.com>
2025-11-19 02:39:07 +0900

    컬링문제네 씨팔

--- LabProject08-1-3/Shader.cpp
+++ LabProject08-1-3/Shader.cpp
@@ -842,3 +842,23 @@ D3D12_DEPTH_STENCIL_DESC CInterfaceShader::CreateDepthStencilState()
 
 	return(d3dDepthStencilDesc);
 }
+
+D3D12_RASTERIZER_DESC CInterfaceShader::CreateRasterizerState()
+{
+	D3D12_RASTERIZER_DESC d3dRasterizerDesc;
+	::ZeroMemory(&d3dRasterizerDesc, sizeof(D3D12_RASTERIZER_DESC));
+
+	d3dRasterizerDesc.FillMode = D3D12_FILL_MODE_SOLID;
+	d3dRasterizerDesc.CullMode = D3D12_CULL_MODE_NONE; // [중요] 컬링을 끕니다 (양면 모두 그림)
+	d3dRasterizerDesc.FrontCounterClockwise = FALSE;
+	d3dRasterizerDesc.DepthBias = 0;
+	d3dRasterizerDesc.DepthBiasClamp = 0.0f;
+	d3dRasterizerDesc.SlopeScaledDepthBias = 0.0f;
+	d3dRasterizerDesc.DepthClipEnable = TRUE;
+	d3dRasterizerDesc.MultisampleEnable = FALSE;
+	d3dRasterizerDesc.AntialiasedLineEnable = FALSE;
+	d3dRasterizerDesc.ForcedSampleCount = 0;
+	d3dRasterizerDesc.ConservativeRaster = D3D12_CONSERVATIVE_RASTERIZATION_MODE_OFF;
+
+	return(d3dRasterizerDesc);
+}
